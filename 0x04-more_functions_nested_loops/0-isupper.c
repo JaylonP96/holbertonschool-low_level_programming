d4370a560d8f9622ffa0725e6d26a64aef0ce62f
JaylonP96 <jaylonp96@gmail.com>
2022-09-22 08:02:18 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,4 +2,14 @@
 
 /**
  * isupper checks for uppercase characters
- * 
+ *@c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+} 
